0afe9241a3b9b4575d29678dce7cf8472c908a2e
r3dph0enix <ipasechnikov@gmail.com>
2023-11-30 12:32:06 +0300

    stings and numbers

--- rwd_cat_photo_app/script.js
+++ rwd_cat_photo_app/script.js
@@ -2,3 +2,13 @@
 /* this is a 
 multi-line comment */
 
+var myName;
+
+//Step 3
+var a;
+a =7;
+// инициализация переменной и сразу присвоение ей значения
+var a = 9;
+//Step 6 String
+var myName = "Ivan";
+var myLastName = "Pasechnikov";
